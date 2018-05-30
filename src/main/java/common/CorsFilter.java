4574f9d32a90e0922b0e351102704d833f903842
xhwanhaoran <whr1314520@qq.com>
2018-05-30 08:46:30 +0800

    CORS配置OPTION

--- src/main/java/common/CorsFilter.java
+++ src/main/java/common/CorsFilter.java
@@ -1,9 +1,11 @@
 package common;
 
 import com.github.pagehelper.util.StringUtil;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -14,6 +16,9 @@ import java.io.IOException;
  */
 @Component
 public class CorsFilter implements Filter {
+
+    public static final String OPTIONS = "OPTIONS";
+    public static final String ACCESS_CONTROL_REQUEST_METHOD = "Access-Control-Request-Method";
 
     private String allowOrigin;
     private String allowMethods;
@@ -41,6 +46,7 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest httpServletRequest = (HttpServletRequest) req;
         if (StringUtil.isNotEmpty(allowOrigin)) {
             //允许访问的客户端域名，例如：http://web.xxx.com，若为*，则表示从任意域都能访问，即不做任何限制；
             response.setHeader("Access-Control-Allow-Origin", allowOrigin);
@@ -54,16 +60,28 @@ public class CorsFilter implements Filter {
             response.setHeader("Access-Control-Allow-Credentials", allowCredentials);
         }
         if (StringUtil.isNotEmpty(allowHeaders)) {
-            //允许服务端访问的客户端请求头，多个请求头用逗号分割，例如：Content-Type,Access-Token,timestamp
+            //允许服务端访问的客户端请求头，多个请求头用逗号分割，例如：Content-Type,X-Token,timestamp
             response.setHeader("Access-Control-Allow-Headers", allowHeaders);
         }
         if (StringUtil.isNotEmpty(exposeHeaders)) {
             //允许客户端访问的服务端响应头，多个响应头用逗号分割。
             response.setHeader("Access-Control-Expose-Headers", exposeHeaders);
         }
+        if (isPreFlightRequest(httpServletRequest)) {
+            response.setStatus(204);
+            return;
+        }
         chain.doFilter(req, res);
     }
 
+    public boolean isCorsRequest(HttpServletRequest request) {
+        return (request.getHeader(HttpHeaders.ORIGIN) != null);
+    }
+
+    public boolean isPreFlightRequest(HttpServletRequest request) {
+        return (isCorsRequest(request) && OPTIONS.equals(request.getMethod())
+                && request.getHeader(ACCESS_CONTROL_REQUEST_METHOD) != null);
+    }
     @Override
     public void destroy() {
 
